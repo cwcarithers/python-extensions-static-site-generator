ac5263bc4eb752201c9d19331c9f10572c09af84
cwcarithers <caritherschristopher@gmail.com>
2022-07-22 21:40:35 -0500

    fix issue

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -5,19 +5,19 @@ start_time = None
 total_written = 0
 
 
-@hooks.filter("start_build")
+@hooks.register("start_build")
 def start_build():
     global start_time
     start_time = time.time()
 
 
-@hooks.filter("written")
+@hooks.register("written")
 def written():
     global total_written
     total_written = total_written + 1
 
 
-@hooks.filter("stats")
+@hooks.register("stats")
 def stats():
     final_time = time.time() - start_time
     average = final_time / total_written if total_written else 0
